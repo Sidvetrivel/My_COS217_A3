5939ac704965c593a40ebf2fd5e9f7e97474b5be
Sidvetrivel <sid.vetri@gmail.com>
2023-03-27 20:29:24 -0400

    void ptr error

--- symtablelist.c
+++ symtablelist.c
@@ -7,7 +7,7 @@
 
 struct Node {
     const char *key;
-    int value;
+    const void* value;
     struct Node *next;
 }; 
 
@@ -30,7 +30,7 @@ void SymTable_free(SymTable_T oSymTable){
     assert(oSymTable != NULL);
     while (free_node != NULL) {
         free((char *)free_node->key);
-        free((int*)free_node->value);
+        free((void *)free_node->value);
         free(free_node);
         free_node = next_node;
     }
